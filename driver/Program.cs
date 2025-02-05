72683a750754c5e77ed362ac07d455e846f5242b
Jan Kotas <jkotas@microsoft.com>
2025-02-05 08:36:54 -0800

    Perf test for string literals

---
+++ driver/Program.cs
@@ -0,0 +1,7 @@
+﻿long startWS = Environment.WorkingSet;
+long startTicks = Environment.TickCount;
+TestLib.Work();
+long endWS = Environment.WorkingSet;
+long endTicks = Environment.TickCount;
+Console.WriteLine($"Cost per string literal (bytes): {(endWS - startWS)/10000}");
+Console.WriteLine($"Cost per string literal (microseconds): {(endTicks - startTicks)/10.0}");
